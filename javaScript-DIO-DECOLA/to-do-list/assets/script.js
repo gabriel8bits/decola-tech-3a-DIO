f45216fa7b1692d6f1b7fe86a9efc2c00a75aa9a
Gabriel <gabrielcpa123@gmail.com>
2022-05-15 19:28:29 -0300

    to-do list

--- javaScript-DIO-DECOLA/to-do-list/assets/script.js
+++ javaScript-DIO-DECOLA/to-do-list/assets/script.js
@@ -1,2 +1,29 @@
 const form = document.getElementById('formulario-tarefa');
-const tarefaLista = document.getElementById('tarefas')
+const tarefaLista = document.getElementById('tarefas');
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const campoInput = document.getElementById('tarefa-input');
+    addTarefa(campoInput.value);
+    form.reset();
+};
+
+function addTarefa(descricao) {
+    const containerTarefa = document.createElement('div');
+    const novaTarefa = document.createElement('input');
+    const tarefaLabel = document.createElement('label');
+    const descricaoTarefa = document.createTextNode(descricao);
+
+    novaTarefa.setAttribute('type', 'checkbox');
+    novaTarefa.setAttribute('name', descricao);
+    novaTarefa.setAttribute('id', descricao);
+
+    tarefaLabel.setAttribute('for', descricao);
+    tarefaLabel.appendChild(descricaoTarefa);
+
+    containerTarefa.classList.add('tarefa-item');
+    containerTarefa.appendChild(novaTarefa);
+    containerTarefa.appendChild(tarefaLabel);
+
+    tarefaLista.appendChild(containerTarefa);
+}
